aa4af5a0ab62dd4dd090013e1d45cd4ee8f33b42
masumkazibd <masum.kazi.hantmk@gmail.com>
2026-09-09 16:14:02 +0600

    bus schedule and route for admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Route, BusSchedule
 
-# Register your models here.
+class BusScheduleInline(admin.TabularInline):
+    model = BusSchedule
+    extra = 1
+
+class RouteAdmin(admin.ModelAdmin):
+    inlines = [BusScheduleInline]
+
+admin.site.register(Route, RouteAdmin)
+admin.site.register(BusSchedule)
